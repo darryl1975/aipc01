8f956e0c89dfc7e071f8359d68516523f7efcf8f
darryl1975 <darryl1975@hotmail.com>
2024-06-28 12:56:37 +0800

    added inline commands

--- Droplet/resources.tf
+++ Droplet/resources.tf
@@ -3,18 +3,34 @@ resource "digitalocean_droplet" "myaipcdemo" {
   region   = "sgp1"
   size     = "s-1vcpu-1gb"
   image    = "ubuntu-20-04-x64"
-  ssh_keys = ["db:36:7c:f9:ba:82:97:89:ac:08:26:38:07:fb:1b:d3"]
+  ssh_keys = ["3a:d8:dc:8b:0d:2b:ae:e9:5b:ab:02:de:b3:0f:97:36"]
 
   connection {
     type        = "ssh"
     user        = "root"
-    private_key = file("~/.ssh/id_ed25519")
+    private_key = file("~/.ssh/id_terraform")
     host        = self.ipv4_address
   }
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt update"
+      "sudo apt update",
+      "wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg",
+      "echo \"deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main\" | sudo tee /etc/apt/sources.list.d/hashicorp.list",
+      "sudo apt update && sudo apt install terraform",
+      "sudo apt update",
+      "sudo apt install ca-certificates curl",
+      "sudo install -m 0755 -d /etc/apt/keyrings",
+      "sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc",
+      "sudo chmod a+r /etc/apt/keyrings/docker.asc",
+      "echo \"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu $(. /etc/os-release && echo \"$VERSION_CODENAME\") stable\" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null",
+      "sudo apt-get update",
+      "sudo apt-get install --assume-yes docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin",
+      "curl -O \"https://gitlab-docker-machine-downloads.s3.amazonaws.com/v0.16.2-gitlab.19/docker-machine-Linux-x86_64\"",
+      "mv docker-machine-Linux-x86_64 docker-machine",
+      "chmod +x docker-machine",
+      "sudo mv docker-machine /usr/local/bin",
+      "docker-machine version"
     ]
 
   }
